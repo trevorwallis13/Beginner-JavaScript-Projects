011204c132347f7bd72ea818a1acbeb407b7e5c4
trevorwallis13 <trevor@walliscreative.com>
2021-11-28 23:30:47 -0600

    Updated variable names and added comments

--- Reviews/app.js
+++ Reviews/app.js
@@ -1,40 +1,44 @@
-const people = [
+// Array with reviews objects
+
+const reviews = [
   {
     id: 1,
-    name: "John Doe",
-    image: "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
-    title: "Content Writer",
-    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst quisque sagittis purus."
+    author: "John Doe",
+    img: "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    job: "Content Writer",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst quisque sagittis purus."
   },
   {
     id: 2,
-    name: "Sally Johnson",
-    image: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
-    title: "Frontend Developer",
-    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet risus nullam eget felis."
+    author: "Sally Johnson",
+    img: "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    job: "Frontend Developer",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet risus nullam eget felis."
   },
   {
     id: 3,
-    name: "Tony Tiger",
-    image: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-    title: "Vice President of Fun",
-    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet."
+    author: "Tony Tiger",
+    img: "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    job: "Vice President of Fun",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet."
   },
   {
     id: 4,
-    name: "Jimmy John",
-    image: "https://www.jimmyjohns.com/images/common/jimmyjohns_logo.png",
-    title: "Sandwich Man",
-    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet."
+    author: "Jimmy John",
+    img: "https://www.jimmyjohns.com/images/common/jimmyjohns_logo.png",
+    job: "Sandwich Man",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet."
   },
   {
     id: 5,
-    name: "Sara Jones",
-    image: "https://images.unsplash.com/photo-1611432579699-484f7990b127?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    title: "UX Designer",
-    review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu."
+    author: "Sara Jones",
+    img: "https://images.unsplash.com/photo-1611432579699-484f7990b127?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    job: "UX Designer",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu."
   }
 ];
+
+// DOM objects
 
 const img = document.getElementById("person-img");
 const author = document.getElementById("author");
@@ -45,54 +49,66 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
-let val = 0;
+// Index placeholder for selecting review objects.
+// Will change the current value to select different reviews
+
+let current = 0;
+
+// Previous button decreases current by 1 and updates DOM
 
 prevBtn.addEventListener("click", () => {
 
-  if(val === 0) {
-    val = people.length - 1;
+  if(current === 0) {
+    current = reviews.length - 1;
   } else {
-    val --;
+    current --;
   };
 
-  console.log(val);
+  let item = reviews[current];
 
-  img.src = people[val].image;
-  author.textContent = people[val].name;
-  job.textContent = people[val].title;
-  info.textContent = people[val].review;
+  img.src = item.img;
+  author.textContent = item.author;
+  job.textContent = item.job;
+  info.textContent = item.info;
 
 })
+
+// Next button increases current and updates DOM
 
 nextBtn.addEventListener("click", () => {
 
-  if(val === people.length - 1) {
-    val = 0;
+  if(current === reviews.length - 1) {
+    current = 0;
   } else {
-    val ++;
+    current ++;
   };
 
-  console.log(val);
+  let item = reviews[current];
 
-  img.src = people[val].image;
-  author.textContent = people[val].name;
-  job.textContent = people[val].title;
-  info.textContent = people[val].review;
+  img.src = item.img;
+  author.textContent = item.author;
+  job.textContent = item.job;
+  info.textContent = item.info;
 
 })
 
+// Random buttom selects a random review from the array
+// New review will be different than current 
+
 randomBtn.addEventListener("click", () => {
 
-  let oldVal = val;
+  let oldVal = current;
 
-  while (oldVal === val) {
-    val = Math.floor(Math.random() * people.length);
+  while (oldVal === current) {
+    current = Math.floor(Math.random() * reviews.length);
   }
 
-  img.src = people[val].image;
-  author.textContent = people[val].name;
-  job.textContent = people[val].title;
-  info.textContent = people[val].review;
+  let item = reviews[current];
+
+  img.src = item.img;
+  author.textContent = item.author;
+  job.textContent = item.job;
+  info.textContent = item.info;
 
 });
 
