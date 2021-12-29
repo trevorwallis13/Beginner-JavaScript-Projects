bd3cc8bf0884aaab24121a5b6eeaa882c23e45fe
trevorwallis13 <trevor@walliscreative.com>
2021-12-28 23:16:50 -0600

    Built stopwatch functionality

--- stopwatch/app.js
+++ stopwatch/app.js
@@ -5,7 +5,7 @@
     displays:   current total time (mm:ss:cs)
                 current lap time (mm:ss:cs)
                 laps
-                    -- lap counter
+                    -- lap totalTimeCount
                     -- lap time
 
 */
@@ -27,12 +27,14 @@ const lapSection = document.querySelector('.laps');
 
 // format time
 
-let counter = 0;
+let totalTimeCount = 0;
+let lapTimeCount = 0;
+let lapNumber = 1;
 
-function formatTime(counter) {
-    let minutes = Math.floor(counter / 6000);
-    let seconds = Math.floor((counter - (minutes * 600)) / 100);
-    let centiSeconds = counter % 100;
+function formatTime(totalTimeCount) {
+    let minutes = Math.floor(totalTimeCount / 6000);
+    let seconds = Math.floor((totalTimeCount - (minutes * 6000)) / 100);
+    let centiSeconds = totalTimeCount % 100;
 
     if(minutes < 10) {minutes = '0' + minutes};
     if(seconds < 10) {seconds = '0' + seconds};
@@ -45,10 +47,12 @@ function formatTime(counter) {
 // start/stop functions
 
 let timerInterval = null;
+let lapInterval = null;
 
 function startTimer() {
     timerInterval = setInterval(() => {
-        totalTimeDisp.textContent = formatTime(counter++);
+        totalTimeDisp.textContent = formatTime(totalTimeCount++);
+        lapTimeDisp.textContent = formatTime(lapTimeCount++);
     }, 10);
 }
 
@@ -62,15 +66,53 @@ function stopTimer() {
 startStopBtn.addEventListener('click', () => {
     if(timerInterval) {
         stopTimer();
+        startStopBtn.textContent = "Start";
     } else {
         startTimer();
+        startStopBtn.textContent = "Stop";
     }
 });
 
 lapBtn.addEventListener('click', () => {
     console.log("Lap button clicked!");
+    
+    let lapTime = formatTime(lapTimeCount);
+    lapTimeCount = 0;
+
+    if(lapTimeCount > 0) {
+        createLapRow(lapNumber, lapTime);
+        lapNumber++;
+    }
 })
 
 resetBtn.addEventListener('click', () => {
-    console.log("Reset button clicked!");
+    if(timerInterval) {stopTimer()};
+
+    totalTimeCount = 0;
+    lapTimeCount = 0;
+    lapNumber = 1;
+
+    totalTimeDisp.textContent = formatTime(totalTimeCount);
+    lapTimeDisp.textContent = formatTime(totalTimeCount);
+
+    lapSection.innerText = "";
+    startStopBtn.innerText = "Start";
 })
+
+// html functions
+
+function createLapRow(lapNumber, lapTime) {
+    const   lapNumberEl = document.createElement("p");
+            lapNumberEl.setAttribute("class", "lap-num");
+            lapNumberEl.innerText = `Lap ${lapNumber}`;
+
+    const   lapTimeEl = document.createElement("p");
+            lapTimeEl.setAttribute("class", "lap-time");
+            lapTimeEl.innerText = lapTime;
+
+    const   lapRowEl = document.createElement("div");
+            lapRowEl.setAttribute("class", "lap-row");
+            lapRowEl.append(lapNumberEl, lapTimeEl);
+
+    lapSection.append(lapRowEl);
+}
